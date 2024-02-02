764d462d83b1a3c6a8146fd8e9b1180f2f9dc5e3
Eshya <achmadayas@gmail.com>
2024-02-03 01:02:22 +0700

    first commit

---
+++ src/main/java/com/eshya/buana_technical_test/BuanaTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.eshya.buana_technical_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuanaTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuanaTechnicalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshya/buana_technical_test/BuanaTechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshya.buana_technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuanaTechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
