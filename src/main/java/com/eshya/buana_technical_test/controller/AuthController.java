e6d57aeb8feffe7299fb4ee18772948d358b5c46
Eshya <achmadayas@gmail.com>
2024-02-03 19:21:04 +0700

    github & google oauth

---
+++ src/main/java/com/eshya/buana_technical_test/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.eshya.buana_technical_test.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class AuthController {
+    @GetMapping("/user")
+    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+//        System.out.println("Reached /user endpoint");
+        if(principal == null)
+            return Collections.singletonMap("name", null);
+        return Collections.singletonMap("name", principal.getAttribute("name"));
+    }
+    @GetMapping("/error")
+    public String error(HttpServletRequest request) {
+        String message = (String) request.getSession().getAttribute("error.message");
+        request.getSession().removeAttribute("error.message");
+        return message;
+    }
+}
